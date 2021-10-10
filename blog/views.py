3447ca91add22776542bb4b9f2564c2d0978b4b0
Tarun <iamtarun24@gmail.com>
2021-10-10 19:02:18 +0530

    Url and template addition

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    pass
+    return render(request,"blog/startpage.html")
 
 def posts(request):
     pass
